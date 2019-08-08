75007d6d903636659a99f19a656153ce41790c76
Antti Salo <antti.salo@sc5.io>
2019-08-08 14:41:41 +0100

    getUserMedia test with start/stop

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
-(function() {
-  console.log('main.js init');
-})();
+var video = document.querySelector("#videoContainer");
+var button = document.querySelector("#startStop");
+
+function videoInit(e) {
+  if (video.srcObject) {
+    stopVideo();
+    button.innerHTML = 'START';
+  } else {
+    startVideo();
+    button.innerHTML = 'STOP';
+  }
+}
+
+function startVideo(e) {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+}
+
+function stopVideo(e) {
+  var stream = video.srcObject;
+  var tracks = stream.getTracks();
+
+  for (var i = 0; i < tracks.length; i++) {
+    var track = tracks[i];
+    track.stop();
+  }
+
+  video.srcObject = null;
+}
+
+startVideo();
